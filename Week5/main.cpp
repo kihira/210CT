ecf14174d28985725837f77494bece1a19b001cd
Kihira <zoe@kihira.uk>
2016-11-16 19:33:56 +0000

    Switch over to shared ptr's because that seems like a good idea

--- Week5/main.cpp
+++ Week5/main.cpp
@@ -1,30 +1,33 @@
 #include <iostream>
 #include <vector>
+#include <memory>
 
 using namespace std;
 
 template <typename N>
 struct elem {
     N value;
-    elem *next;
+    shared_ptr<elem> next;
 };
+
+template <typename N>
+using ElemPtr = shared_ptr<elem<N>>;
 
 template <typename N>
 class CircularLinkedList
 {
 public:
-    elem<N>* first;
-    elem<N>* last;
-    void remove(elem<N>* e)
+    ElemPtr<N> first;
+    ElemPtr<N> last;
+    void remove(ElemPtr<N> e)
     {
-        elem<N>* curr = first;
+        ElemPtr<N> curr = first;
         while (true)
         {
             if (curr->next == e)
             {
-                elem<N>* next = curr->next->next; // need to copy to prevent issues
+                ElemPtr<N> next = curr->next->next; // need to copy to prevent issues
                 curr->next = next;
-                delete e;
                 break;
             }
             curr = curr->next;
@@ -32,28 +35,19 @@ public:
     }
     CircularLinkedList(vector<N> input)
     {
-        this->first = new elem<N>;
-        this->first->value = input[0];
-        this->first->next = this->first;
+        ElemPtr<N> start(new elem<N>);
+        start->value = input[0];
+        start->next = this->first;
+        this->first = start;
         this->last = this->first;
         for (int i = 1; i < input.size(); i++)
         {
-            elem<N> *in = new elem<N>;
+            ElemPtr<N> in(new elem<N>);
             in->value = input[i];
             in->next = this->first;
             this->last->next = in;
             this->last = in;
         }
-    }
-    ~CircularLinkedList()
-    {
-//        elem<N>* curr = this->first;
-//        while (true)
-//        {
-//            elem<N>* temp = curr->next;
-//            delete curr;
-//            curr = temp;
-//        }
     }
 };
 
@@ -63,13 +57,11 @@ int main()
     vector<string> a = {"Eliot", "Charlie", "Zoe", "Alex", "Sophie"};
     CircularLinkedList<string> list(a);
 
-    elem<string>* value = list.last;
-    elem<string>* prev = list.last;
+    ElemPtr<string> value = list.last;
     for (int i = 0; i < a.size()-1; i++)
     {
         for (int j = 0; j < a.size()-1; j++) // Loop around until the next one is found
         {
-            prev = value;
             value = value->next;
         }
         list.remove(value->next);
